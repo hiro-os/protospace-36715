989159fa4f08e3a163bd5952869145d1b354ca06
hiro-os <73930571+hiro-os@users.noreply.github.com>
2022-01-06 22:11:29 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移できないように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -44,9 +44,11 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
-      redirect_to action: :index
+    unless user_signed_in? 
+      @prototype = Prototype.find(params[:id])
+      unless  current_user.id == @prototype.user_id
+        redirect_to action: :index
+      end
     end
   end
 
